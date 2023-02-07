bfe95038f236fcb76ebe674c839f9435c2254f1d
Varghese <JVARGH7@emory.edu>
2023-02-07 09:30:08 -0500

    Changed map references to india shapefiles Added nfhs5 iapr_pregnant and nfhs5 iair pregnant

--- preprocessing/ncpre01_nfhs5 women.R
+++ preprocessing/ncpre01_nfhs5 women.R
@@ -18,4 +18,12 @@ nfhs5_women <- read_dta(paste0(path_dhs_data,"/IA/IAPR7CDT/IAPR7CFL.dta"),col_se
 
 saveRDS(nfhs5_women,paste0(path_cascade_folder,"/working/nfhs5 iapr_women.RDS"))
 
-  
+
+nfhs5_pregnant <- read_dta(paste0(path_dhs_data,"/IA/IAPR7CDT/IAPR7CFL.dta"),col_select = iapr7a_variables$selected) %>% 
+  rename_with(~ iapr7a_variables$new_var[which(iapr7a_variables$selected == .x)], 
+              .cols = iapr7a_variables$selected) %>%
+  dplyr::filter(!is.na(age),age >=18,pregnant == 1) %>% 
+  ncp_preprocessing(.,sex = "Female")
+
+saveRDS(nfhs5_women,paste0(path_cascade_folder,"/working/nfhs5 iapr_women.RDS"))
+saveRDS(nfhs5_pregnant,paste0(path_cascade_folder,"/working/nfhs5 iapr_pregnant.RDS"))
